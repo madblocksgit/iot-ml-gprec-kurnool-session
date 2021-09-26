9ac57ba467e2ab08f4381087fd48c53f73625bd4
MS <72651078+madblocksgit@users.noreply.github.com>
2021-09-26 14:33:23 +0530

    Create remote-bulb-publish.py

---
+++ remote-bulb-publish.py
@@ -0,0 +1,21 @@
+# User (Google Colab) - Publisher (Sender) 
+# Broker - broker.hivemq.com (Server)
+# Bulb (Arduino) - Subscriber (Receiver)
+
+import paho.mqtt.client as mqtt
+
+# create a client object
+client=mqtt.Client()
+
+# connect with broker
+client.connect('broker.hivemq.com',1883)
+print ('Broker Connected')
+
+while True:
+  print('Enter on to switch on the device')
+  print('Enter off to switch off the device')
+  k=input('Enter choice: ')
+  if(k=="on"):
+    client.publish('iot/gprec','on')
+  elif(k=='off'):
+    client.publish('iot/gprec','off')
