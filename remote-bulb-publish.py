780090042d941aac6c0ad70dad07c3c2df5a07cc
MS <72651078+madblocksgit@users.noreply.github.com>
2021-09-26 14:51:29 +0530

    Update remote-bulb-publish.py

--- remote-bulb-publish.py
+++ remote-bulb-publish.py
@@ -7,16 +7,17 @@ import paho.mqtt.client as mqtt
 # create a client object
 client=mqtt.Client()
 
-
+# connect with broker
 
 while True:
-  # connect with broker
   client.connect('broker.hivemq.com',1883)
   print ('Broker Connected')
   print('Enter on to switch on the device')
   print('Enter off to switch off the device')
   k=input('Enter choice: ')
   if(k=="on"):
+    print('Device On')
     client.publish('iot/gprec','on')
   elif(k=='off'):
+    print('Device Off')
     client.publish('iot/gprec','off')
